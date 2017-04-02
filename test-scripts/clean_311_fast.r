d9e23f15ea1987a386a5205b3ec7cac44fb78b1f
Amanda Shih <pandafulmanda@gmail.com>
2017-04-02 09:57:55 -0500

    Hopefully fast r (#2)
    * hopefully fast r
    
    * updates

--- test-scripts/clean_311_fast.r
+++ test-scripts/clean_311_fast.r
@@ -3,6 +3,10 @@
 options <- commandArgs(trailingOnly = TRUE)
 write_filename <- options[1]
 
+process_line <- function (line, pipe_positions, number_of_pipes) {
+  line[pipe_positions[-(number_of_pipes:1)]] <- ';'
+  return(paste(line, collapse = ''))
+}
 
 number_of_pipes <- 0
 file.create(write_filename)
@@ -10,7 +14,13 @@ f <- file("stdin")
 open(f)
 while(length(line <- readLines(f,n=1)) > 0) {
   write_line <- line
+  pipe_positions <- unlist(gregexpr('|', line))
 
+  if (number_of_pipes == 0) {
+    number_of_pipes <- length(pipe_positions)
+  } else {
+    write_line <- process_line(line, pipe_positions, number_of_pipes)
+  }
 
   write(write_line, file = write_filename, append = TRUE)
 }
